e3fa97c0d68a354868afa51a4622b8372697279f
fffilimonov <fffilimonov@yandex.ru>
2015-09-16 10:39:13 +0300

    init

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "github.com/fffilimonov/OCIP_go"
+    "os"
+)
+
+func main() {
+    larg:=len(os.Args)
+    if larg < 2 {
+        ocip.LogErr(nil,"no args")
+        os.Exit (1)
+    }
+    var file string = os.Args[1]
+    Config := ocip.ReadConfig(file)
+    var USERID,MODE,DST string
+    if larg > 2 {
+        MODE = os.Args[2]
+    }
+    if larg > 3 {
+        USERID = ocip.ConcatStr("",os.Args[3],"@spb.swisstok.ru")
+    }
+    if larg > 4 {
+        DST = os.Args[4]
+    }
+    if MODE == "status" {
+        ocip.OCIPsend(Config,"UserCallForwardingAlwaysGetRequest",ocip.ConcatStr("","userId=",USERID))
+    }
+    if MODE == "false" {
+        ocip.OCIPsend(Config,"UserCallForwardingAlwaysModifyRequest",ocip.ConcatStr("","userId=",USERID),"isActive=false")
+    }
+    if MODE == "true" {
+        ocip.OCIPsend(Config,"UserCallForwardingAlwaysModifyRequest",ocip.ConcatStr("","userId=",USERID),"isActive=true",ocip.ConcatStr("","forwardToPhoneNumber=",DST))
+    }
+}
